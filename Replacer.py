ff4eff1d766365509ea6f5731b116aec428d84be
James <unicoder_admin@163.com>
2023-07-08 17:02:48 +0200

    Modify Replacer.py, to make it full functional.

--- Replacer.py
+++ Replacer.py
@@ -8,6 +8,7 @@ import commands
 
 # input: filePath(string),data file: path+file
 # output: list of Entry , one by one. 
+#====================
 def readEntries(filePath):
 	ls = []
 		
@@ -15,42 +16,114 @@ def readEntries(filePath):
 	#fopen = codecs.open(filePath, 'r', encoding='utf-32')
 	ls = fopen.readlines()
 	fopen.close()
-	#debug:
 	
+	#debug:
 	print("Entities: " + str(len(ls)))
-	print(ls)
+	#print(ls)
 	
 	return ls
 
 # input: list of Entry, one by one, to replace the wrong files.
 # output:  Running result.
+#====================
 def replaceThem(ls, notTest):
 	partialCommand = 'cp -r '  # -y maybe?
 	for ent in ls:
 		ent1 = ent.strip()
 		path_l = ent.strip().split('/')
-		path_l = path_l[2:]
+		path_l = path_l[2:]  # filter /mnt/16orig the length is 10.
 		ent2 = ''.join(path_l)
 		command = partialCommand + str(ent1) + ' ' + str(ent2)
+		
 		#debug:
 		print(command)
+
 		if notTest:
-			(result_status, result_output) = commands.getstatusoutput(command)
-			if result_status != 0:
-				print('Error: ', result_output)
+			copy_results = -1
+			copy_result = tryTwiceCopy(command)
+			if copy_result == 0:
+				return 0
 			else:
-				print('File created:', result_output)
-	return 0
+				return -1
 
+#Copy only try twice. The second try with mkdir.
+#input: copy command.
+#output: 0:Succeed; -1:fail;
+#====================
+def tryTwiceCopy(command):
+	try:
+		(cp_status, cp_output) = cpFile(command)
+	except Exception, err:
+		print('Try copy once error: ' + str(Exception) + str(err))
+		return -1
+	if cp_status != 0:
+		print('Cp error: ', cp_output)
+		
+		# ~~Try mkdir:~~
 
+		mkdir = -2
+		
+		try:
+			mkdir = createDirectory(cp_output, ent2)
+		except Exception, err:
+			print('Create dirctory error: ' + str(Exception) + str(err))
+			
+		if mkdir == -1:
+			return -1
+		elif mkdir == 0:
+			try:
+				(cp_status2, cp_output2) = cpFile(command)
+			except Exception, err:
+				print('Try copy twice error: ' + str(Exception) + str(err))
+				return -1
+			if cp_status2 != 0:
+				print('Cp twice error: ", cp_output2)
+			else:
+				print('File created by mkdir: ', cp_output2)
+				return 0
 
+		else:  # mkdir == -2
+			print(In mkdir of replaceThem error, check log.)
+			return -1 
+	else:
+		print('File created: ', cp_output)
+		return 0
+
+#input : command is like cp -r.
+#output: Succeed return (0, ""), fail return (-1, errormsg).
+#====================
+def cpFile(command):
+	(result_status, result_output) = commands.getstatusoutput(command)
+		if result_status != 0:
+			print('Cp error: ', result_output)
+			return (-1, result_output)	
+		else:
+			print('File created: ', result_output)
+			return (0, "")
+
+#====================
+def createDirectory(cp_output, ent2):
+	if true and ent
+	(rsl_status, rsl_output) = commands.getstatusoutput('mkdir -pv ' + str(ent2))
+	if rsl_status != 0:
+		print('Mkdir error: ', rsl_status)
+		return -1
+	else:
+		print('Direcoty created: ', rsl_output)
+		return 0
+
+#====================
 def main(argv):
-	if len(argv) == 1 :
-		print("Lack arguments. Not run. Notice: Place the data-file in /home/cuibe/work/replace/, and invoke it explicitly.")
-		return -1
-	elif len(argv) == 2 and (argv[1] == '--help' or argv[1] == '-h'):
-		print("\nReplacer.py [-t|--exec|-h|--help] datafile \n-h --help   get the help notice.\n--exec   do real copy\n-t   do not real copy,test and verbos it only. ")
-		return -1
+	if len(argv) < 3:
+		if len(argv) == 1 :
+			print("Lack arguments. Not run. Notice: Place the data-file in /home/cuibe/work/replace/, and invoke it explicitly.")
+			return -1
+		elif len(argv) == 2 and (argv[1] == '--help' or argv[1] == '-h'):
+			print("\nReplacer.py [-t|--exec|-h|--help] datafile \n-h --help   get the help notice.\n--exec   do real copy\n-t   do not real copy,test and verbos it only. ")
+			return -1
+		elif len(argv) == 2:
+			print("Bad or lack option. Please reference help to give correct one.")
+			return -1
 	else:
 		if argv[1] == '--exec':
 			notTest = True
@@ -68,19 +141,19 @@ def main(argv):
 		print(filePath)
 		print(argv)
 	except Exception, err:
-		print('Make path error:' + str(Exception) + str(err))
+		print('Make path error: ' + str(Exception) + str(err))
 		
 	entities = []
 	try:
 		entities = readEntries(filePath)
 	except Exception, err:
-		print('Reading file error:' + str(Exception) + str(err))
-	result = 1
+		print('Reading file error: ' + str(Exception) + str(err))
+	result = -1
 	if len(entities) > 0:
 		try:
 			result = replaceThem(entities, notTest)
 		except Exception, err:
-			print('Replacing error:' + str(Exception) + str(err))
+			print('Replacing error: ' + str(Exception) + str(err))
 	else:
 		print("No file to copy, check your source-data file.")
 	print("------------------------------")
