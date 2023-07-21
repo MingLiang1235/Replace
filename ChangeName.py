fc6cda630e8a601eb827a46752e1eef3eee9c0d2
Unicoder <unicoder@sohu.com>
2023-07-22 01:38:23 +0800

    Fix ChangeName folder mark.

--- ChangeName.py
+++ ChangeName.py
@@ -35,7 +35,7 @@ def addHead(ls, notTest, outfile='./git.new-input.txt'):
 	fwrite = codecs.open(outfile, 'w')
 	for each in ls:  # each with '\n'.
 		#each = each.strip()  # Not correct! to strip '\n' ,file.write(line) is not right.
-		line = head + each.strip() + '/\n'  # fixbug(3)
+		line = head + each.strip() + '\n'
 		#debug
 		print(line)
 		if notTest:
